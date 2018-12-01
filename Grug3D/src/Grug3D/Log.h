64df56c4cadb9fd4bc0a8905a697cd18c149df16
AirborneB6 <airborneb6@gmail.com>
2018-12-01 13:59:36 +0200

    No significant change

--- Grug3D/src/Grug3D/Log.h
+++ Grug3D/src/Grug3D/Log.h
@@ -2,7 +2,7 @@
 
 #include "Core.h"
 #include <memory>
-#include "spdlog/spdlog.h"
+#include "spdlog\spdlog.h"
 #include "spdlog\sinks\stdout_color_sinks.h"
 
 namespace Grug3D
@@ -26,10 +26,20 @@ namespace Grug3D
 	};
 }
 
-#define GRUG_CORE_ERROR(...) ::Grug3D::Log::getCoreLogger()->error(__VA_ARGS__)
-#define GRUG_CORE_WARN(...) ::Grug3D::Log::getCoreLogger()->warn(__VA_ARGS__)
-#define GRUG_CORE_INFO(...) ::Grug3D::Log::getCoreLogger()->info(__VA_ARGS__)
-#define GRUG_CORE_TRACE(...) ::Grug3D::Log::getCoreLogger()->trace(__VA_ARGS__)
+//Core log macros
+#define GRUG_CORE_ERROR(...)		::Grug3D::Log::getCoreLogger()->error(__VA_ARGS__)
+#define GRUG_CORE_WARN(...)			::Grug3D::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define GRUG_CORE_INFO(...)			::Grug3D::Log::getCoreLogger()->info(__VA_ARGS__)
+#define GRUG_CORE_TRACE(...)		::Grug3D::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define GRUG_CORE_FATAL(...)		::Grug3D::Log::getCoreLogger()->fatal(__VA_ARGS__)
+
+//Client log macros
+#define GRUG_ERROR(...)		::Grug3D::Log::getClientLogger()->error(__VA_ARGS__)
+#define GRUG_WARN(...)		::Grug3D::Log::getClientLogger()->warn(__VA_ARGS__)
+#define GRUG_INFO(...)		::Grug3D::Log::getClientLogger()->info(__VA_ARGS__)
+#define GRUG_TRACE(...)		::Grug3D::Log::getClientLogger()->trace(__VA_ARGS__)
+#define GRUG_FATAL(...)		::Grug3D::Log::getClientLogger()->fatal(__VA_ARGS__)
+
 
 
 
